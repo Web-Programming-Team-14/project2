9c3ff5e7312e81b6c066e255816fecd450137eb7
SophieNguyen113 <sophiengocnguyen113@gmail.com>
2025-03-30 13:22:09 -0400

    Added pages

---
+++ objective-C/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team 14 - Game Show: Quiz Millionaire</title>
+    <link rel="stylesheet" href="../css/objective-C/style.css">
+</head>
+<body>
+    <header id="header">
+        <div class="header__box">
+            <div class="header__logo"></div>
+            <div id="title">Game Show: Quiz Millionaire</div>
+            <nav class="header__navbar">
+                <a href="../php/pages/index.php" target="_self" rel="noopener noreferrer" class="header__link">Get Started</a>
+            </nav>
+        </div>  
+    </header>
+    <div id="main">
+        <div id="content">
+            <img id="icon" src="../img/logo.png" alt="logo-img">
+            <div class="content__group">
+                <p>Ready to test your knowledge and win big? Click on Get Started to start playing!</p>
+                <div class="button-group">
+                    <a href="../php/pages/index.php" class="btn">Get Started</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="copyright">
+        <p>Copyright &copy; Spring 2025</p>
+        <p>Georgia State University - Web Programming</p>
+        <p>Team 14 - Sophie Nguyen - Grant Boddy - Rida Hameed Syeda</p>
+    </div>
+</body>
+</html>
